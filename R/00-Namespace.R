0212cdae9f5fa735811e0d82cde328da83758973
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-04-07 10:54:23 +0200

    add missing import

--- R/00-Namespace.R
+++ R/00-Namespace.R
@@ -1,5 +1,6 @@
 #' @rawNamespace import(shiny)
 #'
+#' @importFrom data.table data.table rbindlist
 #' @importFrom httr2 req_body_json req_headers req_perform request resp_body_json
 #' @importFrom shinyAce aceEditor
 #' @importFrom shinyjs disable enable
