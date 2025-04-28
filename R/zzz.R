17aa43fa3fd317b5e0d8bf1e2485458a497a9040
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-04-28 13:59:47 +0200

    update message onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -6,14 +6,34 @@ running_in_shinyproxy <- function() {
   tolower(Sys.getenv("IS_SHINYPROXY", "false")) == "true"
 }
 
+ollama_host_reachable <- function(url = "http://host.docker.internal:11434") {
+  req <- request(url) |> req_timeout(1)
+
+  tryCatch({
+    resp <- req_perform(req)
+    TRUE
+  }, error = function(e) {
+    FALSE
+  })
+}
+
 .onLoad <- function(libname, pkgname) {
   if (running_in_docker() && !running_in_shinyproxy()) {
-    Sys.setenv(OLLAMA_BASE_URL = "http://host.docker.internal:11434")
+    if (ollama_host_reachable()) {
+      Sys.setenv(OLLAMA_BASE_URL = "http://host.docker.internal:11434")
+    }
   }
 }
 
 .onAttach <- function(libname, pkgname) {
   if (running_in_docker() && !running_in_shinyproxy()) {
-    packageStartupMessage(sprintf("[%s] Docker environment detected; OLLAMA_BASE_URL set to host.docker.internal.", pkgname))
+    if (ollama_host_reachable()) {
+      packageStartupMessage(sprintf("[%s] Docker detected: using OLLAMA_BASE_URL = host.docker.internal.", pkgname))
+    } else {
+      packageStartupMessage(sprintf(
+        "[%s] Docker detected, but 'host.docker.internal' is not reachable. Cannot set OLLAMA_BASE_URL. Did you forget to add the host when running the container? Please use: 'docker run --add-host=host.docker.internal:host-gateway your-image'?",
+        pkgname
+      ))
+    }
   }
 }
