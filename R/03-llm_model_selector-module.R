4bb2cfb412182d212543788bd3704bd14c4b7f23
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-06-02 14:56:03 +0200

    clean up model_selector module

--- R/03-llm_model_selector-module.R
+++ R/03-llm_model_selector-module.R
@@ -2,55 +2,13 @@
 llm_model_selector_ui <- function(id, label = "Select a Model") {
   ns <- NS(id)
   tagList(
-    # radioButtons(ns("source"), "Model source", choices = c("API", "Local"), inline = TRUE),
-    #
-    # re-organize UI for source <--> provider
-    # if API: -> API key file
-    # if local: check load models UI
-    # for both: select a model
-    # later: pass the selected model to the prompt settings module
-    # remove the selection from the prompt settings module
-    selectInput(ns("model"), label, choices = c("Upload an API key..." = ""))
+    selectInput(ns("model"), label, choices = c("Check provider ..." = ""))
   )
 }
 
 # ---- Server Function ----
 llm_model_selector_server <- function(id, api_reactive) {
   moduleServer(id, function(input, output, session) {
-    get_local_models <- reactive({
-      manager <- new_OllamaModelManager()
-      manager <- update(manager)
-      manager$local_models
-    })
-
-    get_api_models <- reactive({
-      api <- api_reactive()
-      if (!inherits(api, "LlmApi")) return(character(0))
-      get_llm_models(api)
-    })
-
-    # observe({
-    #   models <- switch(input$source,
-    #                    "API" = get_api_models(api_reactive()),
-    #                    "Local" = get_local_models(),
-    #                    character(0)
-    #   )
-    #
-    #   choices <- if (length(models) == 0) {
-    #     c("No models found..." = "")
-    #   } else {
-    #     models
-    #   }
-    #
-    #   updateSelectInput(session, "model", choices = choices)
-    # }) |> bindEvent(input$source, api_reactive())
-    #
-    # # Output: selected model + source
-    # reactive(list(
-    #   source = input$source,
-    #   model = input$model
-    # ))
-
     observe({
       api <- api_reactive()
 
