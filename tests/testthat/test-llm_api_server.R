9b9ac30be1b963a06c92a45a43fbba4d4c729eb8
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-06-13 14:13:27 +0200

    fix test

--- tests/testthat/test-llm_api_server.R
+++ tests/testthat/test-llm_api_server.R
@@ -1,9 +1,6 @@
 testthat::test_that("Test llm_api_server with too short OpenAI key", {
   shiny::testServer(llm_api_server,
-                    args = list(
-                      no_internet = NULL,
-                      exclude_pattern = ""
-                    ),
+                    args = list(no_internet = NULL, exclude_pattern = ""),
                     {
                       # Arrange
                       print("test llm_api_server: OpenAI provider with valid key file")
@@ -13,7 +10,7 @@ testthat::test_that("Test llm_api_server with too short OpenAI key", {
                         provider = "OpenAI",
                         api_key_file = structure(
                           list(
-                            name = "test-importData_rgpt_validKeyFormat.txt",
+                            name = "test-key",
                             size = 10L,
                             type = "text/plain",
                             datapath = testthat::test_path("test-gpt_key_too_short.txt")
@@ -28,10 +25,7 @@ testthat::test_that("Test llm_api_server with too short OpenAI key", {
 
 testthat::test_that("Test llm_api_server with OpenAI key", {
   shiny::testServer(llm_api_server,
-                    args = list(
-                      no_internet = NULL,
-                      exclude_pattern = ""
-                    ),
+                    args = list(no_internet = NULL, exclude_pattern = ""),
                     {
                       # Arrange
                       print("test llm_api_server: OpenAI provider with valid key file")
@@ -41,18 +35,19 @@ testthat::test_that("Test llm_api_server with OpenAI key", {
                         provider = "OpenAI",
                         api_key_file = structure(
                           list(
-                            name = "test-importData_rgpt_validKeyFormat.txt",
+                            name = "test-key",
                             size = 10L,
                             type = "text/plain",
-                            datapath = testthat::test_path("test-gpt_key_format_ok.txt")
+                            datapath = testthat::test_path("test-gpt_key_invalid.txt")
                           ),
                           class = "data.frame",
                           row.names = c(NA, -1L)
                         )
                       )
 
-                      testthat::expect_true(inherits(api(), "RemoteLlmApi"))
-                      testthat::expect_equal(api()$provider, "OpenAI")
-                      testthat::expect_equal(api()$url_models , c(OpenAI = "https://api.openai.com/v1/models"))
+                      testthat::expect_equal(
+                        api(),
+                        structure(list(), error = "API request failed: Unauthorized: API key is invalid or expired.\n• OAuth error\n• realm: OpenAI API")
+                      )
                     })
 })
