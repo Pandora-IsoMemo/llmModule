ff8ac0e0dd2a3ed2a2f1a304a03fe360f853df91
Jan Abel <jan.abel@inwt-statistics.de>
2025-10-29 12:06:16 +0100

    fix RCMD Check - add || to namespace

--- R/00-Namespace.R
+++ R/00-Namespace.R
@@ -2,6 +2,7 @@
 #'
 #' @importFrom data.table data.table rbindlist
 #' @importFrom httr2 req_body_json req_headers req_perform req_timeout request resp_body_json
+#' @importFrom rlang %||%
 #' @importFrom shinyAce aceEditor updateAceEditor
 #' @importFrom shinyjs disable enable
 #' @importFrom shinyTools shinyTryCatch
